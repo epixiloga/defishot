4e210b444a167085290960b35d798c6ff0bc4ac4
epixiloga <hafbenmoussa@gmail.com>
2018-12-26 20:15:48 +0100

    first commit

--- shotGunStories/Program.cs
+++ shotGunStories/Program.cs
@@ -10,6 +10,12 @@ namespace shotGunStories
     {
         static void Main(string[] args)
         {
+            string[] shot = new string[] { "pas de tir", "tir avec main droite", "tir avec main gauche", "tir avec PistoLaserXFire" };
+            int[] dice = new int[] { 1,1,1,0,0,0,2,2,2,3 };
+         
+            int number = new Random().Next(1,11);
+
+            Console.WriteLine(shot[ dice[number-1] ]);
         }
     }
 }
